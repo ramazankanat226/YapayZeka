4421b893dbd34ef76890e129db6897d5f34f0633
ramazankanat226 <147054807+ramazankanat226@users.noreply.github.com>
2024-10-20 14:18:00 +0300

    Update stream_dosyasi.py

--- gogus_kanseri_streamlit/stream_dosyasi.py
+++ gogus_kanseri_streamlit/stream_dosyasi.py
@@ -7,6 +7,9 @@ import os
 # Başlık
 st.title("Göğüs Kanseri Teşhis Uygulaması")
 
+# Dosya yollarını dinamik olarak oluştur
+scaler_path = os.path.join(os.path.dirname(__file__), 'scaler1.pkl')
+model_path = os.path.join(os.path.dirname(__file__), 'lr1.pkl')
 
 # Model ve scaler dosyalarını yükle
 with open("scaler1.pkl", "rb") as file:

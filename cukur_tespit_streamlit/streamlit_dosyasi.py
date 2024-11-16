1f503c69393a22d478e0b893c365e045c4a6dfda
ramazankanat226 <147054807+ramazankanat226@users.noreply.github.com>
2024-11-16 23:42:44 +0300

    Update streamlit_dosyasi.py

--- cukur_tespit_streamlit/streamlit_dosyasi.py
+++ cukur_tespit_streamlit/streamlit_dosyasi.py
@@ -3,6 +3,12 @@ import cv2
 import tempfile
 import os
 from ultralytics import YOLO
+import gdown
+
+# Google Drive'dan dosya indirme
+url = 'https://drive.google.com/uc?export=download&id=1v0cN_V6_zviDbxxHh3UH-AtGcycO8DXq'
+output = 'best.pt'  # İndirilen dosya ismi
+gdown.download(url, output, quiet=False)
 
 # Modeli yükle
 model = YOLO("best.pt")  # Model dosyasının yolu
